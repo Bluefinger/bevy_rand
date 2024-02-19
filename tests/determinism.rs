6fa30e785621f61dc4a742a8ff28a6ebd330740b
Gonçalo Rica Pais da Silva <bluefinger@gmail.com>
2024-02-19 13:02:24 +0100

    fix: Miri imports for determinism test

--- tests/determinism.rs
+++ tests/determinism.rs
@@ -1,7 +1,10 @@
 #![allow(clippy::type_complexity)]
 
 use bevy::prelude::*;
-use bevy_rand::prelude::*;
+use bevy_prng::{ChaCha12Rng, ChaCha8Rng, WyRand};
+use bevy_rand::prelude::{
+    EntropyComponent, EntropyPlugin, ForkableAsRng, ForkableRng, GlobalEntropy,
+};
 use rand::prelude::Rng;
 
 use rand_core::RngCore;
