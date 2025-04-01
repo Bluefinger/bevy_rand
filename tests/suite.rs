c83764b104574e00f3d94b01549a96769df7026d
Gonçalo Rica Pais da Silva <bluefinger@gmail.com>
2025-04-01 19:55:59 +0200

    cargo fmt

--- tests/suite.rs
+++ tests/suite.rs
@@ -4,10 +4,10 @@
 pub mod bevy_math;
 #[path = "integration/determinism.rs"]
 pub mod determinism;
-#[path = "integration/reseeding.rs"]
-pub mod reseeding;
 #[path = "integration/extension.rs"]
 pub mod extension;
+#[path = "integration/reseeding.rs"]
+pub mod reseeding;
 
 #[cfg(target_arch = "wasm32")]
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
