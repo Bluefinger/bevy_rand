d1e022b313318018629c8331a38bfce810147ee5
Gonçalo Rica Pais da Silva <bluefinger@gmail.com>
2025-04-01 15:48:18 +0200

    tests: Add integration tests for extension trait

--- tests/suite.rs
+++ tests/suite.rs
@@ -6,6 +6,8 @@ pub mod bevy_math;
 pub mod determinism;
 #[path = "integration/reseeding.rs"]
 pub mod reseeding;
+#[path = "integration/extension.rs"]
+pub mod extension;
 
 #[cfg(target_arch = "wasm32")]
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
