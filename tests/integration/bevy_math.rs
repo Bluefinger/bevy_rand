6f15e6f53ceb4e26cbe4048e0fbbd6d9b8f36d13
Gonçalo Rica Pais da Silva <bluefinger@gmail.com>
2025-06-19 09:46:10 +0200

    Account for random float variation in Miri tests

--- tests/integration/bevy_math.rs
+++ tests/integration/bevy_math.rs
@@ -17,8 +17,8 @@ fn prng_compatibility() {
     let boundary = circle.sample_boundary(&mut source);
     let interior = circle.sample_interior(&mut source);
 
-    assert_eq!(&boundary, &Vec2::new(-40.885902, 9.609526));
-    assert_eq!(&interior, &Vec2::new(-15.362211, 32.41336));
+    assert!(boundary.abs_diff_eq(Vec2::new(-40.885902, 9.609526), f32::EPSILON * 1000.0));
+    assert!(interior.abs_diff_eq(Vec2::new(-15.362211, 32.41336), f32::EPSILON * 1000.0));
 }
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
@@ -32,8 +32,8 @@ fn component_compatibility() {
             let boundary = circle.sample_boundary(source.as_mut());
             let interior = circle.sample_interior(source.as_mut());
 
-            assert_eq!(&boundary, &Vec2::new(-40.885902, 9.609526));
-            assert_eq!(&interior, &Vec2::new(-15.362211, 32.41336));
+            assert!(boundary.abs_diff_eq(Vec2::new(-40.885902, 9.609526), f32::EPSILON * 1000.0));
+            assert!(interior.abs_diff_eq(Vec2::new(-15.362211, 32.41336), f32::EPSILON * 1000.0));
         })
         .run();
 }
